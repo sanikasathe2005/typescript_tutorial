ceaf7e1ab2b1ec489261e85bf1c4d0e6798a5991
Sanika Sathe <sanika.sathe@parallel-minds.com>
2026-09-01 10:50:00 +0530

    numbers,special numbers,basic operations

---
+++ number.ts
@@ -0,0 +1,33 @@
+
+
+//integer
+
+let marks:number=90;
+console.log(marks);
+let percentage:number=90.90;
+console.log(percentage);
+let temp:number=-98;
+console.log(temp);
+
+//special numbers
+//1.NaN
+let res:number=NaN;
+console.log(res);
+let res2:number=0/0;
+console.log(res2);
+//2.Infinity
+let t=10/0;
+console.log(t);
+
+//arithmetic operations
+let a1:number=10;
+let a2:number=10;
+console.log(a1+a2);
+console.log(a1-a2);
+console.log(a1*a2);
+console.log(a1/a2);
+
+
+
+
+
